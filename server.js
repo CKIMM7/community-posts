bc4fe6e53ce72fed82e27d9d8a03cf5429ff2641
YKIM <dyounggkim@gmail.com>
2022-11-10 08:18:28 +0000

    fixed package.json for deploy

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded());
 app.use(cors());
 
-app.use('/', express.static(path.join(__dirname, 'client')));
+app.use('/', express.static(path.join(__dirname, '/client')));
 
 let reqPath = path.join(__dirname, '/client');
 let errorPath = path.join(__dirname, '/client');
